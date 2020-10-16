75f459e2e093601f1614a3222bf25f5ff2388411
jacordobah <jacordobah@unal.edu.co>
2020-10-16 12:44:55 -0500

    Dos clases modificadas
    paraleer y icfesproyect, puesta inicial

--- ICFES_PROJECT/src/icfes_proJect/ICFES_PROJECT.java
+++ ICFES_PROJECT/src/icfes_proJect/ICFES_PROJECT.java
@@ -18,68 +18,21 @@ public class ICFES_PROJECT {
 
     /**
      * @param args the command line arguments
-     */    private static final Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        // TODO code application logic here
-        queue_using_arrays<String> icf =new queue_using_arrays<>(300000);
-        File archivo = null;
-      FileReader fr = null;
-      BufferedReader br = null;
-
-      try {
-         // Apertura del fichero y creacion de BufferedReader para poder
-         // hacer una lectura comoda (disponer del metodo readLine()).
-         
-         archivo = new File ("C:\\Users\\maleja\\Desktop\\SB11_20182.TXT");
-         fr = new FileReader (archivo);
-         br = new BufferedReader(fr);
-
-         // Lectura del fichero
-         String linea;
-         for(int i=0;i<300000;i++){
-            if((linea=br.readLine())!=null)
-                    icf.enqueue(linea);
-          
-                 }
-         for (int i=0;i<300000;i++){
-            System.out.println(icf.dequeue());           
-        }
-      }
-      catch(Exception e){
-         e.printStackTrace();
-       
-      }finally{
-         // En el finally cerramos el fichero, para asegurarnos
-         // que se cierra tanto si todo va bien como si salta 
-         // una excepcion.
-         try{                    
-            if( null != fr ){   
-               fr.close();     
-            }                  
-         }catch (Exception e2){ 
-            e2.printStackTrace();
-         }
-      }
-        
-        queue_using_arrays<Integer> queue =new queue_using_arrays<>(10);
-        for (int i=0;i<10;i++){
-                       
-            queue.enqueue(i);
-        }
-        
-        for (int i=0;i<10;i++){
-            System.out.println(queue.dequeue());           
-        }
-               
-        System.out.println("hello");
-         Stack_using_arrays<Integer> stack =new  Stack_using_arrays<>(10);
-        for (int i=0;i<10;i++){
-         
-            stack.push(i);
-        }       
-        for (int i=0;i<10;i++){
-            System.out.println(stack.pop());
+     */   
+    public static void main(String[] Args){
+        paraleer pl = new paraleer();
+        System.out.println(pl.existe());
+        //String[] a = pl.returnString().toString();
+        Stack pila = new Stack();
+        String[] aux;
+        //int aux1 = 500000;
+        while((aux = pl.returnString())!=null){//&&aux1-->0){
+            pila.add(aux);
             
         }
-      
-}}
+        int size = pila.size();
+        for (int i = 0;i<size;i++){
+            System.out.println(Arrays.toString((String[])pila.pop()));
+        }   
+    }
+}
--- ICFES_PROJECT/src/icfes_proJect/paraleer.java
+++ ICFES_PROJECT/src/icfes_proJect/paraleer.java
@@ -5,10 +5,63 @@
  */
 package icfes_proJect;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author maleja
  */
 public class paraleer {
+    //private static final Scanner scanner = new Scanner(System.in);
+    File archivo;
+    String[] array;
+    FileReader fr;
+    BufferedReader br;
     
+    
+    
+    paraleer(){
+        this.array = new String[25];
+        try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+         
+         this.archivo = new File ("SB11_2018-2.TXT");
+         this.fr = new FileReader (archivo);
+         this.br = new BufferedReader(fr);
+        }catch (Exception e){
+            System.out.println("Error al leer el archivo");
+            
+        }
+    }
+    
+    public String[] returnString(){
+        
+        String linea;
+        try{
+            linea=this.br.readLine(); 
+            this.array = linea.split(";");
+            return this.array;
+        }catch(Exception e){
+            
+        }
+        return null;
+    }
+    
+    public void close(){
+        try{                    
+            if( null != this.fr ){   
+               this.fr.close();     
+            }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+    }
+    
+    public boolean existe(){
+        return this.archivo.exists();
+    }
 }
